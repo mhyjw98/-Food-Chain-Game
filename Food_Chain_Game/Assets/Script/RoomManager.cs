0d98789203459334be0d4d1ec71bb7553283c2b9
mhyjw98 <rhapsodymis4@naver.com>
2025-02-20 02:50:58 +0900

    feat: RoomPlayer 스크립트 생성

---
+++ Food_Chain_Game/Assets/Script/RoomManager.cs
@@ -0,0 +1,54 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : NetworkRoomManager
+{
+    public static RoomManager Instance { get; private set; }
+
+    private List<GameObject> players = new List<GameObject>();
+
+    public override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        players.Add(conn.identity.gameObject);
+
+        Debug.Log($"플레이어 {conn.connectionId} 입장. 현재 플레이어 수: {players.Count}");
+
+        foreach (var player in players)
+        {
+            Debug.Log($"현재 접속 중: {player.GetComponent<NetworkIdentity>().connectionToClient.connectionId}");
+        }
+    }
+
+    public override void OnServerDisconnect(NetworkConnectionToClient conn)
+    {
+        if(conn.identity != null && conn.identity.gameObject != null)
+        {
+            players.Remove(conn.identity.gameObject);
+            base.OnServerDisconnect(conn);
+        }
+        else
+        {
+            Debug.LogWarning($"플레이어 {conn.connectionId}의 연결이 종료됨.");
+
+            //강제 종료된 경우 players 리스트에서 연결 ID를 기반으로 제거
+            GameObject playerToRemove = players.Find(player => player.GetComponent<NetworkIdentity>().connectionToClient == conn);
+            if (playerToRemove != null)
+            {
+                players.Remove(playerToRemove);
+                Debug.Log($"강제 종료된 플레이어 {conn.connectionId} 삭제 완료. 현재 플레이어 수: {players.Count}");
+            }
+        }
+
+        Debug.Log($"플레이어가 퇴장. 현재 플레이어 수: {players.Count}");
+    }
+}

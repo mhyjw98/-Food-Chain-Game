63fc265a530c19ee3536f7af24167c55c36e4b41
mhyjw98 <rhapsodymis4@naver.com>
2025-04-04 02:40:33 +0900

    Feat: RoomPlayer 기본 세팅

--- Food_Chain_Game/Assets/Script/RoomPlayer.cs
+++ Food_Chain_Game/Assets/Script/RoomPlayer.cs
@@ -11,6 +11,11 @@ public class RoomPlayer : NetworkRoomPlayer
     public override void OnStartClient()
     {
         base.OnStartClient();
+        if (isLocalPlayer)
+        {
+            //Debug.Log("[RoomPlayer] 로컬 플레이어 - 자동 준비 요청");
+            //CmdChangeReadyState(true); // 자동으로 준비 상태로 만듦
+        }
         Debug.Log($"플레이어 {netId}가 방에 입장했습니다.");
     }
 
@@ -18,22 +23,17 @@ public class RoomPlayer : NetworkRoomPlayer
     {
         Debug.Log("내 플레이어가 생성됨");
     }
-
     public override void ReadyStateChanged(bool oldReadyState, bool newReadyState)
     {
         Debug.Log($"플레이어 {netId} 준비 상태 변경: {newReadyState}");
     }
 
-    [Command] // 서버에서만 실행되도록 설정
-    public void CmdSetCharacter(string character)
+    public void SetCharacter(string character)
     {
         assignedCharacter = character;
     }
-
-    private void OnCharacterAssigned(string oldCharacter, string newCharacter)
+    private void OnCharacterAssigned(string oldValue, string newValue)
     {
-        Debug.Log($"내 캐릭터가 배정됨: {newCharacter}");
-        LobbyManager.Instance.UpdateCharacterDisplay(newCharacter); // UI 업데이트
+        Debug.Log($"내 캐릭터가 배정됨: {newValue}");
     }
-
 }

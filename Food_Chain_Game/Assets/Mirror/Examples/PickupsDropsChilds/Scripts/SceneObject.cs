ee75511d2579087e5f86b0f9619459d793f80463
mhyjw98 <rhapsodymis4@naver.com>
2025-05-27 01:03:42 +0900

    Update: Mirror 버전 변경

--- Food_Chain_Game/Assets/Mirror/Examples/PickupsDropsChilds/Scripts/SceneObject.cs
+++ Food_Chain_Game/Assets/Mirror/Examples/PickupsDropsChilds/Scripts/SceneObject.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using UnityEngine;
 
 namespace Mirror.Examples.PickupsDropsChilds
@@ -6,49 +6,30 @@ namespace Mirror.Examples.PickupsDropsChilds
     [RequireComponent(typeof(Rigidbody))]
     public class SceneObject : NetworkBehaviour
     {
+        [Header("Prefabs")]
         public GameObject ballPrefab;
         public GameObject batPrefab;
         public GameObject boxPrefab;
 
+        [Header("Settings")]
+        [Range(0, 5)] public float force = 1;
+
+        // IMPORTANT: Order of SyncVar declarations is intentional!
+        // ChangeEquipment coroutine depends on equippedItemConfig being set first
+        // but equippedItemConfig can also be changed independent of equippedItem
+        // changing, e.g. reloading usages.
+        [Header("SyncVars in Specific Order")]
+        [SyncVar(hook = nameof(OnChangeEquippedItemConfig))]
+        public EquippedItemConfig equippedItemConfig = default;
         [SyncVar(hook = nameof(OnChangeEquipment))]
         public EquippedItem equippedItem;
 
-        void OnChangeEquipment(EquippedItem oldEquippedItem, EquippedItem newEquippedItem)
-        {
-            StartCoroutine(ChangeEquipment(newEquippedItem));
-        }
+        [Header("Diagnostics")]
+        [ReadOnly] public GameObject equippedObject;
+        [ReadOnly] public Vector3 direction;
 
-        // Since Destroy is delayed to the end of the current frame, we use a coroutine
-        // to clear out any child objects before instantiating the new one
-        IEnumerator ChangeEquipment(EquippedItem newEquippedItem)
-        {
-            while (transform.childCount > 0)
-            {
-                Destroy(transform.GetChild(0).gameObject);
-                yield return null;
-            }
-
-            // Use the new value, not the SyncVar property value
-            SetEquippedItem(newEquippedItem);
-        }
-
-        // SetEquippedItem is called on the client from OnChangeEquipment (above),
-        // and on the server from CmdDropItem in the PlayerEquip script.
-        public void SetEquippedItem(EquippedItem newEquippedItem)
-        {
-            switch (newEquippedItem)
-            {
-                case EquippedItem.ball:
-                    Instantiate(ballPrefab, transform);
-                    break;
-                case EquippedItem.bat:
-                    Instantiate(batPrefab, transform);
-                    break;
-                case EquippedItem.box:
-                    Instantiate(boxPrefab, transform);
-                    break;
-            }
-        }
+        // Cached reference to IEquipped component on the child object
+        [ReadOnly, SerializeField] IEquipped iEquipped;
 
         protected override void OnValidate()
         {
@@ -68,13 +49,60 @@ namespace Mirror.Examples.PickupsDropsChilds
             if (TryGetComponent(out Rigidbody rb))
             {
                 rb.isKinematic = false;
-                rb.AddForce(Vector3.forward, ForceMode.Impulse);
+                rb.AddForce(direction * force, ForceMode.Impulse);
             }
         }
 
         void OnMouseDown()
         {
             NetworkClient.localPlayer.GetComponent<PickupsDropsChilds>().CmdPickupItem(gameObject);
+        }
+
+        void OnChangeEquippedItemConfig(EquippedItemConfig _, EquippedItemConfig newEquippedItemConfig)
+        {
+            // equippedItem may be EquippedItem.nothing so check for not null
+            // before getting reference to the IEquipped interface component
+            if (equippedObject != null && equippedObject.TryGetComponent(out iEquipped))
+                if (!iEquipped.equippedItemConfig.Equals(equippedItemConfig))
+                    iEquipped.equippedItemConfig = equippedItemConfig;
+        }
+
+        void OnChangeEquipment(EquippedItem _, EquippedItem newEquippedItem)
+        {
+            StartCoroutine(ChangeEquipment());
+        }
+
+        // Since Destroy is delayed to the end of the current frame, we use a coroutine
+        // to clear out any child objects before instantiating the new one
+        IEnumerator ChangeEquipment()
+        {
+            while (transform.childCount > 0)
+            {
+                Destroy(transform.GetChild(0).gameObject);
+                yield return null;
+            }
+
+            equippedObject = null;
+
+            switch (equippedItem)
+            {
+                case EquippedItem.ball:
+                    equippedObject = Instantiate(ballPrefab, transform);
+                    break;
+                case EquippedItem.bat:
+                    equippedObject = Instantiate(batPrefab, transform);
+                    break;
+                case EquippedItem.box:
+                    equippedObject = Instantiate(boxPrefab, transform);
+                    break;
+            }
+
+            // equippedItem may be EquippedItem.nothing so check for not null
+            // before getting reference to the IEquipped interface component
+            // and only set the equippedItemConfig if it's different.
+            if (equippedObject != null && equippedObject.TryGetComponent(out iEquipped))
+                if (!iEquipped.equippedItemConfig.Equals(equippedItemConfig))
+                    iEquipped.equippedItemConfig = equippedItemConfig;
         }
     }
 }

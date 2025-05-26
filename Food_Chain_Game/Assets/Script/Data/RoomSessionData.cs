c9237f5d6e0779e6ace2d139eee3b15f0103d414
mhyjw98 <rhapsodymis4@naver.com>
2025-05-27 02:02:10 +0900

    Feat: 방 코드 데이터 저장

---
+++ Food_Chain_Game/Assets/Script/Data/RoomSessionData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class RoomSessionData
+{
+    public static string CurrentRoomCode;
+    public static string PreviousHostId;
+}

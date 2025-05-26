908e15d4b8172054d9085a6c47d7467ab2ea16b5
mhyjw98 <rhapsodymis4@naver.com>
2025-05-27 02:17:19 +0900

    Feat: 에러별 메시지 생성 구현

---
+++ Food_Chain_Game/Assets/Script/Network/RoomJoin.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class RoomJoin : MonoBehaviour
+{
+    public string nodeServerUrl = ConfigManager.Config.IP;
+
+    public void CheckRoomBeforeJoin(string code, Action<bool, string> onResult)
+    {
+        StartCoroutine(GetIpFromCode(code, (ip, error) =>
+        {
+            if (!string.IsNullOrEmpty(error))
+            {
+                onResult(false, error);
+                return;
+            }
+
+            Debug.Log($"[RoomJoin] 조회된 IP: {ip}");
+            RoomManager.singleton.networkAddress = ip;
+            onResult(true, null); // 입장 가능
+        }));
+    }
+    IEnumerator GetIpFromCode(string code, Action<string, string> callback)
+    {
+        UnityWebRequest request = UnityWebRequest.Get($"{nodeServerUrl}/lookup/{code}");
+        yield return request.SendWebRequest();
+        
+        if (request.responseCode == 404)
+        {
+            callback(null, "코드가 유효하지 않습니다.");
+            yield break;
+        }
+        else if (request.responseCode == 403) // 인원수 초과
+        {          
+            callback(null, "방 인원이 가득 찼습니다.");
+            yield break;
+        }
+        else if (request.responseCode == 200)
+        {
+            string ip = request.downloadHandler.text;
+            callback(ip, null);
+        }
+        else  if (request.result != UnityWebRequest.Result.Success)
+        {
+            callback(null, "서버와 연결할 수 없습니다.");
+            yield break;
+        }
+        else
+        {
+            callback(null, "문제가 발생했습니다. 나중에 다시 시도해주세요.");
+            yield break;
+        }
+    }
+}

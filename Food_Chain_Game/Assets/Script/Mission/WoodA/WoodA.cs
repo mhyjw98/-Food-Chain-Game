8bdd3f63e52a2908ed73d1ad50bedd5163cd1f77
mhyjw98 <rhapsodymis4@naver.com>
2025-12-17 03:08:36 +0900

    Feat: 나무 미션A 임시 구현
    나무에서 잔가지들을 모두 잘라 수집하면 성공하는 미션

---
+++ Food_Chain_Game/Assets/Script/Mission/WoodA/WoodA.cs
@@ -0,0 +1,162 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
+using UnityEngine;
+
+public class WoodA : BaseMission
+{
+    [SerializeField] private WoodItemA woodPrefab;
+    [SerializeField] private Transform[] woodSpawnPoints;
+    [SerializeField] private RectTransform playPanel;
+    [SerializeField] private Canvas canvas;
+
+    [SerializeField] private TextMeshProUGUI statusText;
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private TextMeshProUGUI progressText;
+    [SerializeField] private float timeLimit = 30f;
+
+    [SerializeField] private int _totalWood = 6;
+
+    private float _remainTime;
+    private bool _isComplete;
+
+    private int _collectCount;    
+
+    private List<WoodItemA> _spawnedItems = new List<WoodItemA>();
+    private void Update()
+    {
+        if (_isComplete) return;
+
+        _remainTime -= Time.deltaTime;
+        if (_remainTime < 0f) _remainTime = 0f;
+
+        if (timerText != null)
+            timerText.text = _remainTime.ToString("0.0");
+
+        if (_remainTime <= 0f && !_isComplete)
+        {
+            StartCoroutine(FailCoroutine());
+        }
+        if (Input.GetMouseButton(0))
+        {
+            SweepCutWood();
+        }
+    }
+
+    public override void Begin()
+    {
+        base.Begin();
+        MissionType = MissionType.Wood_A;
+
+        ClearObject();
+
+        _isComplete = false;
+        _remainTime = timeLimit;
+        _collectCount = 0;
+
+        statusText.text = "";
+
+        SpawnWoods();
+        UpdateProgressUI();
+    }
+    private void ClearObject()
+    {
+        foreach (var item in _spawnedItems)
+        {
+            if (item != null)
+                Destroy(item.gameObject);
+        }
+        _spawnedItems.Clear();
+    }
+    private void SweepCutWood()
+    {
+        if (_spawnedItems == null || _spawnedItems.Count == 0)
+            return;
+
+        Camera cam = canvas.worldCamera;
+
+        Vector2 screenPos = Input.mousePosition;
+
+        foreach (var item in _spawnedItems)
+        {
+            if (item == null) continue;
+            item.TryCutBySweep(screenPos, cam);
+        }
+    }
+    private void SpawnWoods()
+    {
+        if (woodPrefab == null || woodSpawnPoints == null)
+            return;
+
+        foreach (var sp in woodSpawnPoints)
+        {
+            if (sp == null) continue;
+
+            var inst = Instantiate(woodPrefab, playPanel);
+            var instRect = inst.GetComponent<RectTransform>();
+            var spRect = sp as RectTransform;
+
+            Vector3 localPos = playPanel.InverseTransformPoint(sp.position);
+            instRect.localPosition = localPos;
+            instRect.localRotation = sp.rotation;
+
+
+            inst.Init(this, playPanel);
+            _spawnedItems.Add(inst);
+        }
+
+        _totalWood = _spawnedItems.Count;
+    }
+
+    public void OnWoodCollected(WoodItemA item)
+    {
+        if (_isComplete) return;
+        if (item == null) return;
+
+        if (_spawnedItems.Contains(item))
+            _spawnedItems.Remove(item);
+
+        _collectCount++;
+        UpdateProgressUI();
+
+        if (_collectCount >= _totalWood)
+        {
+            StartCoroutine(CompleteCoroutine());
+        }
+    }
+    private void UpdateProgressUI()
+    {
+        if (progressText != null)
+        {
+            progressText.text = $"나뭇가지 수집: {_collectCount} / {_totalWood}";
+        }
+    }
+
+    private IEnumerator CompleteCoroutine()
+    {
+        _isComplete = true;
+
+        statusText.text = "성 공";
+        statusText.color = Color.green;
+        statusText.rectTransform.SetAsLastSibling();
+
+        yield return new WaitForSeconds(2f);
+
+        Complete();
+    }
+
+    private IEnumerator FailCoroutine()
+    {
+        _isComplete = true;
+
+        statusText.text = "실 패";
+        statusText.color = Color.red;
+        statusText.rectTransform.SetAsLastSibling();
+
+        yield return new WaitForSeconds(2f);
+
+        Fail();
+    }
+}
---
+++ Food_Chain_Game/Assets/Script/Mission/WoodA/WoodItemA.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class WoodItemA : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler
+{
+    [SerializeField] private float dropDuration = 1f;
+    [SerializeField] private float minRotation = -35f;
+    [SerializeField] private float maxRotation = 35f;
+    [SerializeField]
+    private AnimationCurve dropCurve
+        = AnimationCurve.EaseInOut(0, 0, 1, 1);
+
+    [SerializeField] private float floorOffsetMin = 20f;
+    [SerializeField] private float floorOffsetMax = 150f;
+    [SerializeField] private float horizontalJitter = 20f;
+
+    private RectTransform _rect;
+    private RectTransform _playPanel;
+    private WoodA _mission;
+    private bool _cut;
+    public void Init(WoodA mission, RectTransform playPanel)
+    {
+        _mission = mission;
+        _playPanel = playPanel;
+
+        if (_rect == null)
+            _rect = GetComponent<RectTransform>();
+
+        _cut = false;
+    }
+
+    private void Awake()
+    {
+        if (_rect == null)
+            _rect = GetComponent<RectTransform>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        TryCut();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        TryCut();
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        TryCut();
+    }
+
+    public void TryCutBySweep(Vector2 screenPos, Camera cam)
+    {
+        if (_cut) return;
+        if (_rect == null) return;
+
+        bool inside = RectTransformUtility.RectangleContainsScreenPoint(
+            _rect,
+            screenPos,
+            cam
+        );
+
+        if (!inside) return;
+
+        TryCut();
+    }
+    private void TryCut()
+    {
+        if (_cut) return;
+        _cut = true;
+
+        StartCoroutine(CutAndCollectCoroutine());
+    }
+
+    private IEnumerator CutAndCollectCoroutine()
+    {
+        Vector2 startPos = _rect.anchoredPosition;
+
+        float floorY = startPos.y - 100f;
+
+        if (_playPanel != null)
+        {
+            Rect r = _playPanel.rect;
+            float baseFloor = r.yMin;
+
+            float offset = Random.Range(floorOffsetMin, floorOffsetMax);
+            floorY = baseFloor + offset;
+        }
+
+        Vector2 endPos = new Vector2(
+            startPos.x + Random.Range(-horizontalJitter, horizontalJitter),
+            floorY
+        );
+
+        Quaternion rotStart = _rect.localRotation;
+        float targetZ = Random.Range(minRotation, maxRotation);
+        Quaternion rotEnd = Quaternion.Euler(0f, 0f, targetZ);
+
+        float t = 0f;
+        float duration = Mathf.Max(0.01f, dropDuration);
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime / duration;
+            float a = Mathf.Clamp01(t);
+
+            float eased = dropCurve != null
+                ? dropCurve.Evaluate(a)
+                : (1f - Mathf.Pow(1f - a, 2f));
+
+            _rect.anchoredPosition = Vector2.Lerp(startPos, endPos, eased);
+            _rect.localRotation = Quaternion.Slerp(rotStart, rotEnd, eased);
+
+            yield return null;
+        }
+
+        _rect.anchoredPosition = endPos;
+        _rect.localRotation = rotEnd;
+
+        if (_mission != null)
+            _mission.OnWoodCollected(this);
+    }
+}
+
